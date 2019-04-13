4e70c4e4e642cfb74e9810fe916a5f30cb7a9c77
Steven Contreras <steve.a.contreras@gmail.com>
2019-04-12 22:36:59 -0700

    still not seeing junit test results in travis

--- src/test/java/com/sacontreras/library/datastructures/common/KVPs.java
+++ src/test/java/com/sacontreras/library/datastructures/common/KVPs.java
@@ -9,8 +9,8 @@ public class KVPs {
     static {
         try {
             KVPs.ary_kvp_ssn_name_expected_inorder = Generics.<CKeyValuePair<String, String>>newBoxedTypeArray(5);
-            KVPs.ary_kvp_ssn_name_expected_inorder[0] = new BoxedType<CKeyValuePair<String, String>>(new CKeyValuePair<String, String>("123456789", "Steven Contreras"));
-            KVPs.ary_kvp_ssn_name_expected_inorder[1] = new BoxedType<CKeyValuePair<String, String>>(new CKeyValuePair<String, String>("012345678", "A Mistake!"));
+            KVPs.ary_kvp_ssn_name_expected_inorder[0] = new BoxedType<CKeyValuePair<String, String>>(new CKeyValuePair<String, String>("012345678", "Steven Contreras"));
+            KVPs.ary_kvp_ssn_name_expected_inorder[1] = new BoxedType<CKeyValuePair<String, String>>(new CKeyValuePair<String, String>("123456789", "A Mistake!"));
             KVPs.ary_kvp_ssn_name_expected_inorder[2] = new BoxedType<CKeyValuePair<String, String>>(new CKeyValuePair<String, String>("-1878723", "ksdjsdkj ksjdksjkd ,m"));
             KVPs.ary_kvp_ssn_name_expected_inorder[3] = new BoxedType<CKeyValuePair<String, String>>(new CKeyValuePair<String, String>("9309u08u2034", "fjdnflkndfmnskdjvn sdsx"));
             KVPs.ary_kvp_ssn_name_expected_inorder[4] = new BoxedType<CKeyValuePair<String, String>>(new CKeyValuePair<String, String>("94t824398r", "fsdfs"));
